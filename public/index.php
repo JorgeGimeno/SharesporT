2d6fb969da2cb9738888973e01645e8535eb69cd
Alex <algeorgios.web@gmail.com>
2019-08-07 13:33:38 +0200

    Scripts

--- public/index.php
+++ public/index.php
@@ -77,9 +77,7 @@ require_once('header.php');
     require_once('scripts.php');
 ?>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 
 </body>
 
