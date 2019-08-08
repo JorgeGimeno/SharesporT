882c36d5e7ce0412a7492798274d6415b82a778d
Carlos <carlos.dom13@hotmail.com>
2019-08-08 13:16:47 +0200

    commit log in front-end terminado

--- public/log_in.php
+++ public/log_in.php
@@ -7,10 +7,10 @@ require_once('header.php');
   <header class="masthead text-center text-white">
     <div class="masthead-content">
       <div class="container">
-        <label>Inicio de sesion</label>
-        <input type="text" class="form-control " placeholder="Nombre de usuario" 
+        <h1>Inicio de sesion</h1>
+        <input type="text" class="form-control w-50 mx-auto" placeholder="Nombre de usuario" 
                     aria-label="Nombre de usuario" aria-describedby="basic-addon1">
-        <input type="text" class="form-control mt-2" placeholder="Contraseña" 
+        <input type="text" class="form-control mt-2 w-50 mx-auto" placeholder="Contraseña" 
                     aria-label="Contraseña" aria-describedby="basic-addon1">
         <button class="btn btn-primary btn-xl rounded-pill mt-5">Iniciar Sesion</button>
       </div>
