c44b5ca68e033642bfb27c96f7714315f31418e0
Alex <algeorgios.web@gmail.com>
2019-08-08 13:57:24 +0200

    Alex

--- public/footer.php
+++ public/footer.php
@@ -1,5 +1,5 @@
   <!-- Footer -->
-  <footer class="py-5 bg-black fixed-bottom">
+  <footer class="py-3 bg-black">
     <div class="container">
       <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2019</p>
     </div>
