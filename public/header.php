b7fd6cc426ac5bc39e80f5d96bce8a781b38a5d5
Carlos <carlos.dom13@hotmail.com>
2019-08-08 13:50:11 +0200

    commit cambios log in

--- public/header.php
+++ public/header.php
@@ -27,14 +27,14 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="navbar-brand" href="index.php">Start Bootstrap</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">Sign Up</a>
+            <a class="nav-link" href="registro.php">Sign Up</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="log_in.php">Log In</a>
