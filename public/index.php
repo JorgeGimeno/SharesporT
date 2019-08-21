9d943e2d9f79cee2a6ff56934ce54b7ab14b8f1a
Jose <josegracia92@gmail.com>
2019-08-21 13:50:50 +0200

    Arreglo botones carousel 21/08

--- public/index.php
+++ public/index.php
@@ -7,9 +7,9 @@ require_once('header.php');
   <header class="masthead text-center text-white">
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+      <li data-target="#carouselExampleControls" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
