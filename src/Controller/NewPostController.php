76835c12c000ae6e405fd8a7d81e0f02fb998194
Carlos <carlos.dom13@hotmail.com>
2019-08-26 13:52:03 +0200

    progreso new_post

--- src/Controller/NewPostController.php
+++ src/Controller/NewPostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use DateTime;
 use App\Entity\StPosts;
 use App\Form\StPostsType;
+use App\Entity\StDeportes;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,25 +18,36 @@ class NewPostController extends AbstractController
     public function index(Request $req)
     {
 
-        
+        //Obtenemos la lista de deportes
+        $listaDeportes = $this->getDoctrine()
+            ->getRepository(StDeportes::class)
+            ->findAll();
 
+        //creamos un objeto post
         $newPost = new StPosts();
         $form = $this->createForm(StPostsType::class, $newPost);
         $form->handleRequest($req);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) { 
+
             $newPost->setFecha(new DateTime());
-            $newPost->setIdUsuario("1");
-            $newPost->setIdDeporte("2");
+            //$newPost->setContenido($req->request->get('contenido'));
+            $newPost->setIdUsuario($this->getUser()->getId());
+            $newPost->setIdDeporte(2);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($newPost);
             $entityManager->flush();
 
-            return $this->redirectToRoute('main');
+            return $this->redirectToRoute('new_post');
+             
+        } else {
+            return $this->render('new_post/index.html.twig', [
+                'deportes' => $listaDeportes,
+                'form' => $form->createView()
+            ]);
         }
 
-        return $this->render('new_post/index.html.twig', [
-            'controller_name' => 'NewPostController',
-        ]);
     }
+
 }
--- src/Form/StPostsType.php
+++ src/Form/StPostsType.php
@@ -6,17 +6,16 @@ use App\Entity\StPosts;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class StPostsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('fechaHora')
-            ->add('contenido')
-            ->add('idUsuario')
-            ->add('idDeporte')
-            ->add('idPostPadre')
+            ->add('contenido', TextareaType::class)
+            ->add('idDeporte', ChoiceType::class)
         ;
     }
 
