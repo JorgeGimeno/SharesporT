f52bb8e75db40ba76627cb807d98d8d8ade63798
Jose <josegracia92@gmail.com>
2019-08-27 13:15:51 +0200

    userpanel hardcode

--- src/Controller/UserPanelController.php
+++ src/Controller/UserPanelController.php
@@ -9,12 +9,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class UserPanelController extends AbstractController
 {
     /**
-     * @Route("/userpanel/{id}", name="userpanel", methods={"GET"})
+     * @Route("/userpanel", name="userpanel", methods={"GET"})
      */
-    public function traerUsuarios(int $id)
+    public function traerUsuarios()
     {
         $p = $this->getDoctrine()->getRepository(StUsuarios::class);
-        $idResult = $p->findOneBy(array('id'=>$id));
+        $idResult = $p->findOneBy(array('id'=>201));
 
         return $this->render('user_panel/index.html.twig', [
             'Usuario' => $idResult,
