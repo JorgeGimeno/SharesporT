08fddaaa3bbbe34ba081ebe5fbc59b6f380c8465
Jose <josegracia92@gmail.com>
2019-08-27 13:01:28 +0200

    controlador y vista userpanel

--- src/Controller/UserPanelController.php
+++ src/Controller/UserPanelController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\StUsuarios;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserPanelController extends AbstractController
 {
     /**
-     * @Route("/userpanel", name="userpanel")
+     * @Route("/userpanel/{id}", name="userpanel", methods={"GET"})
      */
-    public function index()
+    public function traerUsuarios(int $id)
     {
+        $p = $this->getDoctrine()->getRepository(StUsuarios::class);
+        $idResult = $p->findOneBy(array('id'=>$id));
+
         return $this->render('user_panel/index.html.twig', [
-            'controller_name' => 'UserPanelController',
+            'Usuario' => $idResult,
             
 
         ]);
