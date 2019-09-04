15f8537c2f8bf7a404be4a9005cd67b32b034163
alumno <alumno@alumnohiberus.com>
2019-09-04 11:48:05 +0200

    testcagadas

--- src/Controller/AdministracionController.php
+++ src/Controller/AdministracionController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class AdministracionController extends AbstractController
 {
     /**
      * @Route("/administracion", name="administracion")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index()
     {
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/main/{currentPage}", name="main")
-     * @IsGranted("ROLE_USER")
+     * 
      */
     public function index($currentPage = 1)
     {
-      
+        
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
             'currentPage' => $currentPage
